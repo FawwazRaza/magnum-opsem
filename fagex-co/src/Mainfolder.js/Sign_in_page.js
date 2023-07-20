6de904f171119b93cd1e68ff9ee358f4999fee68
muahmmadfawwaz <muhammadfawwaz2021@gmail.com>
2023-07-20 11:17:33 +0500

    SignIn_page

--- fagex-co/src/Mainfolder.js/Sign_in_page.js
+++ fagex-co/src/Mainfolder.js/Sign_in_page.js
@@ -3,24 +3,39 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-
-
+import Input from '@mui/joy/Input';
+import Stack from '@mui/joy/Stack';
+import LinearProgress from '@mui/joy/LinearProgress';
+import Typography from '@mui/joy/Typography';
+import { fontSize, fontWeight } from '@mui/system';
 function Signin_page()
 {
-
+const [value, setValue] = React.useState('');
+  const minLength = 12;
     return(
     <>
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="md">
+    
       <Box className="frontboxcss">
+        <form>      
+      <div className='emailinput-fields'>
+<label><span style={{fontWeight: "bold"}}>Email:</span></label><br></br>
+<Input placeholder="Username@gmail.com" variant="outlined" type='email' />
+</div>
+<div className='password-input-fields'>
+<label><span style={{fontWeight: "bold"}}>Password:</span></label><br></br>
+<Input placeholder="Password..." variant="outlined" type='password'/>
+</div>
+</form>
+
+   <div className='center_button'>
       <div class="center">
   <a href="#" class="button">
     <div class="button__line"></div>
     <div class="button__line"></div>
-    <span class="button__text">Submit</span>
+    <span class="button__text">Cancel</span>
   </a>
 
 </div>
@@ -28,9 +43,10 @@ function Signin_page()
 <a href="#" class="button">
     <div class="button__line"></div>
     <div class="button__line"></div>
-    <span class="button__text">Cancel</span>
+    <span class="button__text">Sign In</span>
   </a>
 
+</div>
 </div>
 
       </Box>
